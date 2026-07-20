beb385e7d3264018ac384aa8ca6e1aee900228f5
ItsMeViipeR <itsmeviiper@gmail.com>
2026-07-20 21:15:58 +0200

    fix: change home manager version to 26.05
    Change version to match NixOS's version

--- flake.nix
+++ flake.nix
@@ -3,15 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
 	  ./configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+
+	    home-manager.users."viiper" = import ./home.nix;
+	  }
         ];
       };
     };
