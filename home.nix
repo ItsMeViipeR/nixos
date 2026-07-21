0c026e39980b7b70df5113036f40d7a488e4a396
ItsMeViipeR <itsmeviiper@gmail.com>
2026-07-21 10:36:56 +0200

    fix: finally

--- home.nix
+++ home.nix
@@ -25,33 +25,33 @@
       };
       modules = [
         # SYSTEM (Les clés avec 🐧/⚙️ prennent peu d'octets)
-        { type = "title"; key = " 🚀 fastfetch       "; keyColor = "yellow"; }
-        { type = "os"; key = " ├── 🐧 Linux       "; keyColor = "yellow"; format = "{3} {12}"; }
-        { type = "kernel"; key = " ├── ⚙️  Kernel      "; keyColor = "yellow"; }
-        { type = "shell"; key = " ├── 🐚 Shell       "; keyColor = "yellow"; }
-        { type = "terminal"; key = " ├── 💻 Terminal    "; keyColor = "yellow"; }
-        { type = "packages"; key = " └── 📦 Packages    "; keyColor = "yellow"; }
+        { type = "title"; key = " 🚀 fastfetch               "; keyColor = "yellow"; }
+        { type = "os"; key = " ├── 🐧 Linux               "; keyColor = "yellow"; format = "{3} {12}"; }
+        { type = "kernel"; key = " ├── ⚙️  Kernel              "; keyColor = "yellow"; }
+        { type = "shell"; key = " ├── 🐚 Shell               "; keyColor = "yellow"; }
+        { type = "terminal"; key = " ├── 💻 Terminal            "; keyColor = "yellow"; }
+        { type = "packages"; key = " └── 📦 Packages            "; keyColor = "yellow"; }
         "break"
 
         # HARDWARE (Les emojis 🧠/🎮/📺 font 4 octets UTF-8, on retire 1 ou 2 espaces pour compenser)
         { type = "custom"; format = " 🛠️  Hardware"; outputColor = "blue"; }
-        { type = "cpu"; key = " ├── 🧠 CPU         "; keyColor = "blue"; format = "{1}"; }
-        { type = "gpu"; key = " ├── 🎮 GPU         "; keyColor = "blue"; format = "{2}"; }
-        { type = "display"; key = " ├── 📺 Display     "; keyColor = "blue"; format = "{1}x{2} in {3}\", {11} Hz"; }
-        { type = "memory"; key = " ├── 📊 RAM         "; keyColor = "blue"; }
-        { type = "disk"; key = " ├── 💾 Disk        "; keyColor = "blue"; folders = "/"; }
-        { type = "disk"; key = " └── 💾 Disk        "; keyColor = "blue"; folders = "/home"; }
+        { type = "cpu"; key = " ├── 🧠 CPU                 "; keyColor = "blue"; format = "{1}"; }
+        { type = "gpu"; key = " ├── 🎮 GPU                 "; keyColor = "blue"; format = "{2}"; }
+        { type = "display"; key = " ├── 📺 Display             "; keyColor = "blue"; format = "{width}x{height} in {inch}\", {refresh-rate} Hz"; }
+        { type = "memory"; key = " ├── 📊 RAM                 "; keyColor = "blue"; }
+        { type = "disk"; key = " ├── 💾 Disk (/)            "; keyColor = "blue"; folders = "/"; }
+        { type = "disk"; key = " └── 💾 Disk (/mnt/Jeux)    "; keyColor = "blue"; folders = "/mnt/Jeux"; }
         "break"
 
         # NETWORK (L'icône NerdFont 󰩟 prend aussi plus d'octets)
         { type = "custom"; format = " 🌐 Network"; outputColor = "cyan"; }
-        { type = "localip"; key = " ├── 󰩟  Local IP    "; keyColor = "cyan"; showIpv4 = true; showIpv6 = false; }
-        { type = "dns"; key = " └── 󰒋  DNS         "; keyColor = "cyan"; }
+        { type = "localip"; key = " ├── 󰩟  Local IP            "; keyColor = "cyan"; showIpv4 = true; showIpv6 = false; }
+        { type = "dns"; key = " └── 󰒋  DNS                 "; keyColor = "cyan"; }
         "break"
 
         # DEVELOPMENT
         { type = "custom"; format = " 🦫 Development"; outputColor = "magenta"; }
-        { type = "uptime"; key = " └── 󰔚  Uptime      "; keyColor = "magenta"; }
+        { type = "uptime"; key = " └── 󰔚  Uptime              "; keyColor = "magenta"; }
         "break"
         "colors"
       ];
